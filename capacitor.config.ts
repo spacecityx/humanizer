55d83453cc288266bf47781d9fd43ed8bf48c14f
spacecityx <shawn@spacecityx.com>
2026-04-19 23:38:15 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,8 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.spacecityx.humanizer',
-  appName: 'Humanizer',
-  webDir: '.',
-  server: {
-    androidScheme: 'https'
+{
+  "appId": "com.spacecityx.humanizer",
+  "appName": "Humanizer",
+  "webDir": ".",
+  "server": {
+    "androidScheme": "https"
   }
-};
-
-export default config;
+}
